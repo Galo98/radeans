2c44fb583826238e0284483cf9b02e523680e6c2
Keltoi <galonahuel98@gmail.com>
2023-04-25 15:49:59 -0300

    inicio del proyecto

---
+++ database/insertsRadeans.sql
@@ -0,0 +1,3 @@
+insert into roles (rol_descripcion) values ('administrador'),('cliente');
+
+insert into usuarios (usu_nombre,usu_apellido,usu_email,usu_tel,usu_codArea,usu_contraseña,rol_id) values('admin','','radeans.com.ar@gmail.com','25008282','11','1234',1);
---
+++ readme.md
@@ -0,0 +1,1 @@
+# radeans.com.ar
