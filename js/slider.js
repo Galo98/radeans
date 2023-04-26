40d5e2e746af212c755ead5f7ba92a85d37cc78f
Keltoi <galonahuel98@gmail.com>
2023-04-26 14:23:24 -0300

    feature double sliders

---
+++ js/slider.js
@@ -0,0 +1,73 @@
+const slider = document.querySelector(".slider"),
+    firstImg = slider.querySelectorAll("img")[0];
+
+flechas = document.querySelectorAll(".contenedorSlider i");
+
+let drag = false, isDraggin, prevPageX, prevScrollLeft, positionDiff;
+let firstImgWidth = firstImg.clientWidth + 14; // Agarramos el width de la primera imagen y le agregamos 14 px de margen
+
+flechas.forEach(icon => {
+    icon.addEventListener("click", () => {
+        slider.scrollLeft += icon.id == "left" ? -firstImgWidth : firstImgWidth;
+        showHideIcons();
+        setTimeout(() => showHideIcons(), 60);
+    })
+});
+
+const autoSlide = () => {
+    if (slider.scrollLeft == (slider.scrollLeft - slider.clientWidth)) return;
+
+    positionDiff = Math.abs(positionDiff); // hace los valores positivos
+    let firstImgWidth = firstImg.clientWidth + 14;
+    let valDifference = firstImgWidth - positionDiff;
+
+    if (slider.scrollLeft > prevScrollLeft) {
+        return slider.scrollLeft += positionDiff > firstImgWidth / 2 ? valDifference : -positionDiff;
+    }
+    slider.scrollLeft -= positionDiff > firstImgWidth / 2 ? valDifference : -positionDiff;
+}
+
+const showHideIcons = () => {
+    // Muestra o esconde las flechas de movimiento seguin la posicion del slide
+    let scrollWidth = slider.scrollWidth - slider.clientWidth; // Toma el maximo width scrolleable
+    flechas[0].style.display = slider.scrollLeft == 0 ? "none" : "block";
+    flechas[1].style.display = slider.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+const activar = (e) => {
+    // Actualizacion de las variables globales cuando se hace click
+    drag = true;
+    prevPageX = e.pageX || e.touches[0].pageX;
+    prevScrollLeft = slider.scrollLeft;
+}
+
+const parar = () => {
+    drag = false;
+    slider.classList.remove("dragging");
+    if (!isDraggin) return;
+    isDraggin = false;
+    autoSlide();
+}
+
+const arrastrar = (e) => {
+    // Movimiento de las imagenes en el slider
+    if (!drag) return;
+    e.preventDefault();
+    isDraggin = true;
+    slider.classList.add("dragging");
+    positionDiff = (e.pageX || e.touches[0].pagex) - prevPageX;
+    slider.scrollLeft = prevScrollLeft - positionDiff;
+    showHideIcons()
+}
+
+slider.addEventListener("mousedown", activar)
+slider.addEventListener("touchstart", activar)
+
+slider.addEventListener("mouseup", parar)
+slider.addEventListener("touchmove", parar)
+
+slider.addEventListener("mousemove", arrastrar);
+
+
+slider.addEventListener("mouseleave", parar)
+slider.addEventListener("touchend", parar)
---
+++ js/slider2.js
@@ -0,0 +1,73 @@
+const slider2 = document.querySelector(".slider2"),
+    firstImg2 = slider2.querySelectorAll("img")[0];
+
+flechas2 = document.querySelectorAll(".contenedorSlider2 i");
+
+let drag2 = false, isDraggin2, prevPageX2, prevScrollLeft2, positionDiff2;
+let firstImg2Width = firstImg2.clientWidth + 14; // Agarramos el width de la primera imagen y le agregamos 14 px de margen
+
+flechas2.forEach(icon => {
+    icon.addEventListener("click", () => {
+        slider2.scrollLeft += icon.id == "left" ? -firstImg2Width : firstImg2Width;
+        showHideIcons2();
+        setTimeout(() => showHideIcons2(), 60);
+    })
+});
+
+const autoSlide2 = () => {
+    if (slider2.scrollLeft == (slider2.scrollLeft - slider2.clientWidth)) return;
+
+    positionDiff2 = Math.abs(positionDiff2); // hace los valores positivos
+    let firstImg2Width = firstImg2.clientWidth + 14;
+    let valDifference = firstImg2Width - positionDiff2;
+
+    if (slider2.scrollLeft > prevScrollLeft2) {
+        return slider2.scrollLeft += positionDiff2 > firstImg2Width / 2 ? valDifference : -positionDiff2;
+    }
+    slider2.scrollLeft -= positionDiff2 > firstImg2Width / 2 ? valDifference : -positionDiff2;
+}
+
+const showHideIcons2 = () => {
+    // Muestra o esconde las flechas2 de movimiento seguin la posicion del slide
+    let scrollWidth = slider2.scrollWidth - slider2.clientWidth; // Toma el maximo width scrolleable
+    flechas2[0].style.display = slider2.scrollLeft == 0 ? "none" : "block";
+    flechas2[1].style.display = slider2.scrollLeft == scrollWidth ? "none" : "block";
+}
+
+const activar2 = (e) => {
+    // Actualizacion de las variables globales cuando se hace click
+    drag2 = true;
+    prevPageX2 = e.pageX || e.touches[0].pageX;
+    prevScrollLeft2 = slider2.scrollLeft;
+}
+
+const parar2 = () => {
+    drag2 = false;
+    slider2.classList.remove("dragging");
+    if (!isDraggin2) return;
+    isDraggin2 = false;
+    autoSlide2();
+}
+
+const arrastrar2 = (e) => {
+    // Movimiento de las imagenes en el slider2
+    if (!drag2) return;
+    e.preventDefault();
+    isDraggin2 = true;
+    slider2.classList.add("dragging");
+    positionDiff2 = (e.pageX || e.touches[0].pagex) - prevPageX2;
+    slider2.scrollLeft = prevScrollLeft2 - positionDiff2;
+    showHideIcons2()
+}
+
+slider2.addEventListener("mousedown", activar2)
+slider2.addEventListener("touchstart", activar2)
+
+slider2.addEventListener("mouseup", parar2)
+slider2.addEventListener("touchmove", parar2)
+
+slider2.addEventListener("mousemove", arrastrar2);
+
+
+slider2.addEventListener("mouseleave", parar2)
+slider2.addEventListener("touchend", parar2)
