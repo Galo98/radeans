f03d451744631ed8cef4583fbcaad32a5cbb9834
galo98 <galonahuel98@gmail.com>
2023-06-11 12:06:25 -0300

    estructura menu

--- header.php
+++ header.php
@@ -11,6 +11,35 @@
     </ul>
 </nav>
 <div class="navLog">
-    <a href="login.php" class="logAcc">iniciar sesion</a>
-    <a href="registro.php" class="logReg">registrarse</a>
+    <?php switch ($_SESSION['rol']) {
+        default: ?>
+            <a href="login.php" class="logAcc">iniciar sesion</a>
+            <a href="registro.php" class="logReg">registrarse</a>
+        <?php break;
+        case 1: /* Administrador */ ?>
+            <div>
+                <p>Menu</p>
+                <div>
+                    <a href="">Gestionar Profesionales</a>
+                    <a href="">Gestionar Turnos</a>
+                    <a href="">Lista de turnos</a>
+                    <a href="">Cerrar Sesion</a>
+                </div>
+            </div>
+        <?php break;
+        case 2: /* Cliente */ ?>
+            <div>
+                <p>Menu</p>
+                <div>
+                    <a href="">Perfil</a>
+                    <a href="">Reservar</a>
+                    <a href="">Calificar</a>
+                    <a href="">Cerrar Sesion</a>
+                </div>
+            </div>
+    <?php break;
+    } ?>
+
+
+
 </div>
