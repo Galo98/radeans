6ad961a1dc0ac66e835610162a629625c8e868c8
galo98 <galonahuel98@gmail.com>
2023-06-17 16:45:12 -0300

    menu desplegable admin y clientes

--- header.php
+++ header.php
@@ -10,38 +10,29 @@
         <li><a href="#contacto">contacto</a></li>
     </ul>
 </nav>
-<div class="navLog">
-
-    <?php if (isset($_SESSION['rol'])) {
-        switch ($_SESSION['rol']) {
-            case 1: /* Administrador */ ?>
-                <div>
-                    <p>Menu</p>
-                    <div>
+    <?php if (isset($_SESSION['rol'])) { ?>
+        <div class="dropmenu">
+            <div class="menuBTN">menu <i class="fa-solid fa-chevron-down"></i></div>
+            <div class="cajaMenu">
+                <?php switch ($_SESSION['rol']) {
+                    case 1: /* Administrador */ ?>
                         <a href="">Gestionar Profesionales</a>
                         <a href="">Gestionar Turnos</a>
                         <a href="">Lista de turnos</a>
                         <a href="">Cerrar Sesion</a>
-                    </div>
-                </div>
-            <?php break;
-            case 2: /* Cliente */ ?>
-                <div>
-                    <p>Menu</p>
-                    <div>
+                    <?php break;
+                    case 2: /* Cliente */ ?>
                         <a href="">Perfil</a>
                         <a href="">Reservar</a>
                         <a href="">Calificar</a>
                         <a href="">Cerrar Sesion</a>
-                    </div>
-                </div>
-        <?php break;
-        }
-    } else { ?>
-        <a href="login.php" class="logAcc">iniciar sesion</a>
-        <a href="registro.php" class="logReg">registrarse</a>
+                <?php break;
+                } ?>
+            </div>
+        </div>
+    <?php } else { ?>
+        <div class="navLog">
+            <a href="login.php" class="logAcc">iniciar sesion</a>
+            <a href="registro.php" class="logReg">registrarse</a>
+        </div>
     <?php } ?>
-
-
-
-</div>
