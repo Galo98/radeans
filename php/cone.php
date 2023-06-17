e95dd08f95c78d7f891f912cae2c44648d95b9c7
galo98 <galonahuel98@gmail.com>
2023-06-16 22:53:38 -0300

    fix variable dbname en el die

--- php/cone.php
+++ php/cone.php
@@ -6,7 +6,7 @@ $username="root";
 $password="";
 $dbname="radeans";
 
-$conn=mysqli_connect($servername,$username,$password,$dbname)or die ("No se pudo conectar a la $bdname");
+$conn=mysqli_connect($servername,$username,$password,$dbname)or die ("No se pudo conectar a la $dbname");
 
 return $conn;
 }
